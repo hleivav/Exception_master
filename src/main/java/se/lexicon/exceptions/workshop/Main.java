85c0fa664b679d49aa9ad4cdec082631469082d4
Hernan Leiva <hleiva@hotmail.com>
2025-03-05 22:01:42 +0100

    Reader_Writer done

---
+++ src/main/java/se/lexicon/exceptions/workshop/Main.java
@@ -0,0 +1,33 @@
+package se.lexicon.exceptions.workshop;
+
+import java.io.IOException;
+import java.util.List;
+
+import se.lexicon.exceptions.workshop.data_access.NameService;
+import se.lexicon.exceptions.workshop.domain.Person;
+import se.lexicon.exceptions.workshop.fileIO.CSVReader_Writer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        //TODO:
+
+		List <String> maleFirstNames = CSVReader_Writer.getMaleFirstNames();
+        List <String> femaleFirstNames = CSVReader_Writer.getFemaleFirstNames();
+        List <String> lastNames = null;
+        try {
+            lastNames = CSVReader_Writer.getLastNames();
+        } catch (IOException e){
+            System.out.println("Error occurred trying to read the file.");
+        }
+
+
+
+        NameService nameService = new NameService(maleFirstNames, femaleFirstNames,lastNames);
+
+
+        Person test = nameService.getNewRandomPerson();
+        System.out.println(test);
+
+	}
+
+}
